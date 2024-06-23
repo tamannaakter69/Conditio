2335748d9c9c68dae7b126cb2d6a4907ad57fd41
tamannaakter69 <tamannaakter6998@gmail.com>
2024-06-23 11:51:34 +0600

    multiple Condition

--- totalMarks.js
+++ totalMarks.js
@@ -4,4 +4,13 @@ let math = 100
     islam = 82 
     general_knowledge = 90
 let totalMarks = math + bangla + islam +  general_knowledge
-console.log(totalMarks)
+
+if (totalMarks>300){
+    console.log("Average good Result")
+}else if (totalMarks>350){
+    console.log("woderful Result")
+}else if ( totalMarks>360){
+    console.log("Excellent result")
+}else{
+    console.log("Failed")
+}
