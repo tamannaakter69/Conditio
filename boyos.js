b8ee8c1d5baec65d283b6734f403a2d7a4eae227
tamannaakter69 <tamannaakter6998@gmail.com>
2024-06-23 14:02:28 +0600

    Product

---
+++ boyos.js
@@ -0,0 +1,19 @@
+const age = 18
+const boyoshKal = "Kishor"
+if(age < 1){
+    console.log("Nobojatok shishu")
+}else if (age < 7){
+    console.log("shishu")
+}else if(age >8){
+    console.log("kishor hobe")
+}else if(age>17){
+    console.log("adult")
+}else if ( age>50){
+    console.log("Probin citizen")
+}else if( age>70){
+    console.log("besi bura")
+}else if(age>95){
+    console.log("morte morte beache jacche")
+}else{
+    console.log("moira gease")
+}
---
+++ product.js
@@ -0,0 +1,21 @@
+const mobile = "Tecno"
+switch(mobile){
+    case "Samsung" :
+        console.log("Good service");
+    break;
+    case "Readme" :
+        console.log("In reasonable price they give us good service")
+    break;
+    case "Oppo" :
+        console.log("good camera service");
+    break;
+    case "One plus" :
+        console.log("Baje quality");
+    break; 
+    case "Tecno" :
+        console.log("1 year valoi chole pore somosssa dekha deay..")
+    break;
+
+
+
+}
